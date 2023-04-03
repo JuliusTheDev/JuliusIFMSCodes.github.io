121afa887217b4d19075e34b50f23d53ee25aa5b
JuliusTheDev <117752371+JuliusTheDev@users.noreply.github.com>
2023-04-02 20:04:45 -0400

    Create Atividade1.java

---
+++ Atividade1.java
@@ -0,0 +1,21 @@
+/* Crie um programa que exiba na saída as seguintes mensagens:
+a. Comentários na linguagem Java iniciam com /* e terminam com */
+/* b. Letras minúsculas e maiúsculas são diferentes na linguagem Java
+c. A palavra chave main indica o início do programa na linguagem Java
+d. Os símbolos { e } envolvem um bloco de comandos na linguagem Java
+e. Todas as instruções na linguagem Java devem terminar com um ponto e
+vírgula */ 
+
+public class Atividade1 {
+    public static void main(String[] args) {
+
+        System.out.println(" Comentários na linguagem Java iniciam com /* e terminam com */");
+        System.out.println(" Letras minúsculas e maiúsculas são diferentes na linguagem Java.");
+        System.out.println(" A palavra chave main indica o início do programa na linguagem Java.");
+        System.out.println(" Os símbolos { e } envolvem um bloco de comandos na linguagem Java.");
+        System.out.println(" Todas as instruções na linguagem Java devem terminar com um ponto e vírgula.");
+
+    }
+}
+
+
